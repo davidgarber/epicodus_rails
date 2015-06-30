3dee87ac600b23a2feda83263804026a8dba066c
David Garber & Jordan Carlson <student@epicodus.com>
2015-06-30 10:24:39 -0700

    Create a new section.

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -1,6 +1,28 @@
 class SectionsController < ApplicationController
   def index
     @sections = Section.all
-    render :index
+  end
+
+  def show
+    @section = Section.find(params[:id])
+  end
+
+  def new
+    @section = Section.new
+    render :new
+  end
+
+  def create
+    @section = Section.new(section_params)
+    if @section.save
+      redirect_to sections_path
+    else
+      render :new
+    end
+end
+
+  private
+  def section_params
+    params.require(:section).permit(:title)
   end
 end
